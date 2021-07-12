25d33c743b7bf76008a1cf0487aef1061070b5db
S L <bokuwakami9711@gmail.com>
2021-07-12 03:24:47 +0000

    Fixed emotion output table

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,15 +3,20 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-        let emotions = Object.entries(this.props.emotions).map(([key, value]) => {
-            return <tr><td>{key}</td><td>{value}</td></tr>
-        })
-
         return (  
             <div>
                 <table className="table table-bordered">
                     <tbody>
-                    { emotions }
+                    { 
+                        Object.entries(this.props.emotions).map(([key, value]) => {
+                            return (
+                                <tr>
+                                <td>{key}</td>
+                                <td>{value}</td>
+                                </tr>
+                            )
+                        })
+                    }
                     </tbody>
                 </table>
                 </div>
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
 const app = new express();
-
 app.use(express.static('client'))
-
+ 
 const cors_app = require('cors');
 app.use(cors_app());
+
+const dotenv = require('dotenv');
+const { request } = require('express');
+dotenv.config();
+const api_key = process.env.API_KEY;
+const api_url = process.env.API_URL;
 
 app.get("/",(req, res)=>{
     res.render('index.html');
@@ -15,8 +18,6 @@ app.get("/",(req, res)=>{
 // Creates an instance of NaturalLanguageUnderstanding using the credentials from the
 // from the .env file using the dotenv package and returns the instance
 function getNLUInstance() {
-    let api_key = process.env.API_KEY;
-    let api_url = process.env.API_URL;
 
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
@@ -32,8 +33,6 @@ function getNLUInstance() {
 }
 
 app.get("/url/emotion", (req, res) => {
-    //return res.send({"happy":"90","sad":"10"});
-
     const analyzeParams = {
         'url': req.query.url,
         'features': {
@@ -59,8 +58,6 @@ app.get("/url/emotion", (req, res) => {
 });
 
 app.get("/url/sentiment", (req, res) => {
-    //return res.send("url sentiment for "+req.query.url);
-
     const analyzeParams = {
         'url': req.query.url,
         'features': {
@@ -113,8 +110,6 @@ app.get("/text/emotion", (req,res) => {
 });
 
 app.get("/text/sentiment", (req,res) => {
-    //return res.send("text sentiment for "+req.query.text);
-
     const analyzeParams = {
         'text': req.query.text,
         'features': {
